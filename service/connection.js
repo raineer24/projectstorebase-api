a0ab8f2fcbb6905d7d86ed3f2e81aa072736c2f6
Norbert Aluba Dela Pena <norbert@sg.ibm.com>
2017-11-26 04:55:02 +0800

    Add mysql-model and backbone libraries

--- service/connection.js
+++ service/connection.js
@@ -1,10 +1,10 @@
-const Client = require('mariasql');
 const config = require('../config/config');
 // const log = require('color-logs')(true, true, __filename);
+const sqlModel = require('mysql-model');
 
-module.exports = new Client({
+module.exports = sqlModel.createConnection({
   host: config.db.hostname,
   user: config.db.username,
   password: config.db.password,
-  db: config.db.name
+  database: config.db.name
 });
