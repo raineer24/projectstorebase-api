c719813b9f66072183e81701e9aa4aa55b068189
Norbert Aluba Dela Pena <norbert@sg.ibm.com>
2017-12-04 02:53:54 +0800

    Add register and update seller module

--- api/sellers/index.js
+++ api/sellers/index.js
@@ -1,7 +1,7 @@
 // const BluePromise = require('bluebird');
 // const Conn = require('../../service/connection');
 const Seller = require('./seller');
-// const query = require('../../service/query');
+const query = require('../../service/query');
 
 const seller = {};
 
@@ -22,6 +22,43 @@ seller.registerAccount = (req, res) => {
     .then(id => res.json({ id, message: 'Saved' }))
     .catch(err => res.status(err === 'Found' ? 201 : 500).json({
       message: err === 'Found' ? 'Existing' : 'Failed',
+    }));
+};
+
+/**
+* Update seller
+* @param {Object} req
+* @param {Object} res
+* @return {Object}
+*/
+seller.updateAccount = (req, res) => {
+  const objSeller = new Seller({
+    id: query.validateParam(req.swagger.params, 'id', 0),
+    username: req.swagger.params.body.value.email,
+    password: req.swagger.params.body.value.password,
+    email: req.swagger.params.body.value.email,
+    name: req.swagger.params.body.value.name,
+  });
+  objSeller.update()
+    .then(status => res.json({ status, message: 'Updated' }))
+    .catch(err => res.status(err === 'Not Found' ? 404 : 500).json({
+      message: err === 'Not Found' ? 'Not found' : 'Failed',
+    }));
+};
+
+
+/**
+* View user profile
+* @param {Object} req
+* @param {Object} res
+* @return {Object}
+*/
+seller.viewProfile = (req, res) => {
+  const objSeller = new Seller();
+  objSeller.getById(query.validateParam(req.swagger.params, 'id', 0))
+    .then(result => res.json(Seller.cleanResponse(result, { message: 'Found' })))
+    .catch(() => res.status(404).json({
+      message: 'Not found',
     }));
 };
 
--- api/sellers/seller.js
+++ api/sellers/seller.js
@@ -3,31 +3,101 @@ const Conn = require('../../service/connection');
 // const Util = require('../helpers/util');
 const lodash = require('lodash');
 
-var that;
+let that;
+
+/**
+  * Seller constructor
+  * @param {object} seller
+  * @return {object}
+*/
 
 function Seller(seller) {
   this.model = lodash.extend(seller, {
     dateCreated: new Date().getTime(),
     dateUpdated: new Date().getTime(),
   });
-  this.tableName = 'sellerAccount';
-  this.dbConn = BluePromise.promisifyAll(new Conn({ tableName: this.tableName }));
+  this.table = 'sellerAccount';
+  this.dbConn = BluePromise.promisifyAll(new Conn({ tableName: this.table }));
 
   that = this;
 }
 
-Seller.prototype.create = () => {
-  const DbModel = Conn.extend({ tableName: that.tableName });
-  that.dbConn = BluePromise.promisifyAll(new DbModel(that.model));
-  return new BluePromise((resolve, reject) => {
-    that.dbConn.saveAsync()
-      .then((response) => {
-        resolve(response.insertId);
-      })
-      .catch((err) => {
-        reject(err);
-      });
-  });
+/**
+  * Save Seller account
+  * @return {object}
+*/
+Seller.prototype.create = () => new BluePromise((resolve, reject) => {
+  that.getByValue(that.model.username, 'username')
+    .then((results) => {
+      if (results.length === 0) {
+        const DbModel = Conn.extend({ tableName: that.table });
+        that.dbConn = BluePromise.promisifyAll(new DbModel(that.model));
+        that.dbConn.saveAsync()
+          .then((response) => {
+            resolve(response.insertId);
+          })
+          .catch((err) => {
+            reject(err);
+          });
+      } else {
+        reject('Found');
+      }
+    })
+    .catch((err) => {
+      reject(err);
+    });
+});
+
+/**
+  * Update Seller account
+  * @return {object}
+*/
+Seller.prototype.update = () => new BluePromise((resolve, reject) => {
+  that.getById(that.model.id)
+    .then((results) => {
+      if (!results.id) {
+        reject('Not Found');
+      } else {
+        const DbModel = Conn.extend({ tableName: that.table });
+        that.dbConn = BluePromise.promisifyAll(new DbModel(that.model));
+        that.dbConn.setAsync('id', that.model.id);
+        that.dbConn.saveAsync()
+          .then((response) => {
+            resolve(response.message);
+          })
+          .catch((err) => {
+            reject(err);
+          });
+      }
+    })
+    .catch((err) => {
+      reject(err);
+    });
+});
+
+/**
+  * Get sellerAccount by id
+  * @param {integer} id
+  * @return {object<Promise>}
+*/
+Seller.prototype.getById = id => that.dbConn.readAsync(id);
+
+
+/**
+  * Get sellerAccount by value
+  * @param {any} value
+  * @param {string} field
+  * @return {object<Promise>}
+*/
+Seller.prototype.getByValue = (value, field) => that.dbConn.findAsync('all', { where: `${field} = '${value}'` });
+
+Seller.cleanResponse = (object, properties) => {
+  // eslint-disable-next-line
+  delete object.password;
+  lodash.merge(object, properties);
+
+  return object;
 };
+
 
 module.exports = Seller;
