dbfe66bdb5ed5523fffae472ccb94b690bb84b80
Norbert Aluba Dela Pena <norbert@sg.ibm.com>
2017-12-08 03:18:44 +0800

    Add more dbs

--- db/create.sql
+++ db/create.sql
@@ -12,7 +12,6 @@ email CHAR(100),
 dateCreated BIGINT(50) NOT NULL,
 dateUpdated BIGINT(50) NOT NULL,
 uiid CHAR(150),
-merchant_id BIGINT(50) NOT NULL,
 PRIMARY KEY (id)) ENGINE=InnoDB DEFAULT CHARSET=utf16
 
 /* userProfile */
@@ -33,7 +32,7 @@ phoneMobile CHAR(20),
 phoneHome CHAR(20),
 dateCreated BIGINT(50) NOT NULL,
 dateUpdated BIGINT(50) NOT NULL,
-userAccount_id BIGINT(50) NOT NULL,
+userAccount_id VARCHAR(50) NOT NULL,
 PRIMARY KEY (id)) ENGINE=InnoDB DEFAULT CHARSET=utf16
 
 /* userEntitlement */
@@ -50,31 +49,126 @@ PRIMARY KEY (id)) ENGINE=InnoDB DEFAULT CHARSET=utf16
 /* merchantAccount */
 CREATE TABLE grocerystore.merchantAccount
 (id BIGINT(50) NOT NULL AUTO_INCREMENT,
-name VARCHAR(250),
+name VARCHAR(250) NOT NULL,
+enabled BIT DEFAULT 1,
 dateCreated BIGINT(50) NOT NULL,
 dateUpdated BIGINT(50) NOT NULL,
-enabled BIT(1) DEFAULT 1,
 PRIMARY KEY (id)) ENGINE=InnoDB DEFAULT CHARSET=utf16
 
 /* sellerAccount */
-CREATE TABLE grocerystore.sellerAccount (
-	id bigint(19) NOT NULL auto_increment,
-	username char(60) NOT NULL,
-	password varchar(250) NOT NULL,
-	email char(100) DEFAULT 'NULL',
-	name varchar(250) NOT NULL,
-	dateCreated bigint(19) NOT NULL,
-	dateUpdated bigint(19) NOT NULL,
-	merchant_id bigint(19) DEFAULT 0,
-	PRIMARY KEY (id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf16;
-
-/* sellerEntitlement */
-CREATE TABLE grocerystore.sellerEntitlement
+CREATE TABLE grocerystore.sellerAccount
 (id BIGINT(50) NOT NULL AUTO_INCREMENT,
-type CHAR(50),
+username CHAR(60) NOT NULL,
+password VARCHAR(250) NOT NULL,
+email CHAR(100),
+name VARCHAR(250) NOT NULL,
+dateCreated BIGINT(50) NOT NULL,
+dateUpdated BIGINT(50) NOT NULL,
+merchantAccount_id BIGINT(50) NOT NULL,
+PRIMARY KEY (id)) ENGINE=InnoDB DEFAULT CHARSET=utf16
+
+/* sellerTag */
+CREATE TABLE grocerystore.sellerTag
+(id BIGINT(50) NOT NULL AUTO_INCREMENT,
+name CHAR(60) NOT NULL,
 enabled BIT DEFAULT 1,
-dateCreated BIGINT NOT NULL,
-dateUpdated BIGINT NOT NULL,
-sellerAccount_id BIGINT(50) NOT NULL,
+dateCreated BIGINT(50) NOT NULL,
+dateUpdated BIGINT(50) NOT NULL,
+PRIMARY KEY (id)) ENGINE=InnoDB DEFAULT CHARSET=utf16
+
+/* category */
+CREATE TABLE grocerystore.category
+(id BIGINT(50) NOT NULL AUTO_INCREMENT,
+name CHAR(60) NOT NULL,
+enabled BIT DEFAULT 1,
+dateCreated BIGINT(50) NOT NULL,
+dateUpdated BIGINT(50) NOT NULL,
+PRIMARY KEY (id)) ENGINE=InnoDB DEFAULT CHARSET=utf16
+
+/* item */
+CREATE TABLE grocerystore.item
+(id BIGINT(50) NOT NULL AUTO_INCREMENT,
+name VARCHAR(300) NOT NULL,
+price CHAR(30) NOT NULL,
+displayPrice CHAR(30) NOT NULL,
+availableOn BIGINT(50) NOT NULL,
+slug TINYTEXT NOT NULL,
+enabled BIT DEFAULT 1,
+dateCreated BIGINT(50) NOT NULL,
+dateUpdated BIGINT(50) NOT NULL,
+PRIMARY KEY (id)) ENGINE=InnoDB DEFAULT CHARSET=utf16
+
+/* itemTag */
+CREATE TABLE grocerystore.itemTag
+(id BIGINT(50) NOT NULL AUTO_INCREMENT,
+item_id BIGINT(50) NOT NULL,
+tag_id BIGINT(50) NOT NULL,
+enabled BIT DEFAULT 1,
+dateCreated BIGINT(50) NOT NULL,
+dateUpdated BIGINT(50) NOT NULL,
+PRIMARY KEY (id)) ENGINE=InnoDB DEFAULT CHARSET=utf16
+
+/* itemCategory */
+CREATE TABLE grocerystore.itemCategory
+(id BIGINT(50) NOT NULL AUTO_INCREMENT,
+enabled BIT DEFAULT 1,
+dateCreated BIGINT(50) NOT NULL,
+dateUpdated BIGINT(50) NOT NULL,
+item_id BIGINT(50) NOT NULL,
+category_id BIGINT(50) NOT NULL,
+PRIMARY KEY (id)) ENGINE=InnoDB DEFAULT CHARSET=utf16
+
+/* order */
+CREATE TABLE grocerystore.order
+(id BIGINT(50) NOT NULL AUTO_INCREMENT,
+number CHAR(30) NOT NULL,
+itemTotal CHAR(30) NOT NULL,
+total CHAR(30) NOT NULL,
+shipmentTotal CHAR(30),
+adjustmentTotal CHAR(30),
+paymentTotal CHAR(30),
+dateCompleted BIGINT(50),
+shipmentStatus CHAR(20),
+paymenttStatus CHAR(20),
+email CHAR(100),
+specialInstructions TINYTEXT,
+includedTaxTotal CHAR(100),
+additionalTaxTotal CHAR(100),
+displayIncludedTaxTotal CHAR(100),
+displayAdditionalTaxTotal CHAR(100),
+taxTotal CHAR(100),
+currency CHAR(10),
+totalQuantity CHAR(5),
+token CHAR(150),
+billingAddress01 VARCHAR(250),
+billingAddress02 VARCHAR(250),
+shippingAddress01 VARCHAR(250),
+shippingAddress02 VARCHAR(250),
+dateCreated BIGINT(50) NOT NULL,
+dateUpdated BIGINT(50) NOT NULL,
+userAccount_id BIGINT(50) NOT NULL,
+PRIMARY KEY (id)) ENGINE=InnoDB DEFAULT CHARSET=utf16
+
+
+/* sellerAccount */
+CREATE TABLE grocerystore.sellerAccount
+(id BIGINT(50) NOT NULL AUTO_INCREMENT,
+username CHAR(60) NOT NULL,
+password VARCHAR(250) NOT NULL,
+email CHAR(100),
+name VARCHAR(250) NOT NULL,
+dateCreated BIGINT(50) NOT NULL,
+dateUpdated BIGINT(50) NOT NULL,
+merchantAccount_id BIGINT(50) NOT NULL,
+PRIMARY KEY (id)) ENGINE=InnoDB DEFAULT CHARSET=utf16
+
+/* orderItem */
+CREATE TABLE grocerystore.orderItem
+(id BIGINT(50) NOT NULL AUTO_INCREMENT,
+name CHAR(60) NOT NULL,
+enabled BIT DEFAULT 1,
+dateCreated BIGINT(50) NOT NULL,
+dateUpdated BIGINT(50) NOT NULL,
+order_id BIGINT(50) NOT NULL,
+item_id BIGINT(50) NOT NULL,
 PRIMARY KEY (id)) ENGINE=InnoDB DEFAULT CHARSET=utf16
