be281dbed8955428b5faa70dd15ec83aca9f9617
Norbert Aluba Dela Pena <norbert@sg.ibm.com>
2017-12-18 12:21:04 +0800

    Add findAll method for item model

--- api/items/item.js
+++ api/items/item.js
@@ -1,7 +1,36 @@
-const Item = {
-  getAllView(items, filters) {
-    return filters;
-  },
-};
+const BluePromise = require('bluebird');
+const lodash = require('lodash');
+const Conn = require('../../service/connection');
+const Query = require('../../service/query');
+// const Util = require('../helpers/util');
+
+
+let that;
+
+/**
+  * Item constructor
+  * @param {object} item
+  * @return {object}
+*/
+function Item(item) {
+  this.model = lodash.extend(item, {
+    dateCreated: new Date().getTime(),
+    dateUpdated: new Date().getTime(),
+  });
+  this.table = 'item';
+  this.dbConn = BluePromise.promisifyAll(new Conn({ tableName: this.table }));
+
+  that = this;
+}
+
+/**
+  * findAll
+  * @param {string} limit
+  * @param {string} offset
+  * @param {object} condition
+  * @return {object}
+*/
+Item.prototype.findAll = (offset, limit) => that.dbConn.queryAsync(Query.composeQuery(that.table, ['id', 'name'], null, limit, offset));
+
 
 module.exports = Item;
