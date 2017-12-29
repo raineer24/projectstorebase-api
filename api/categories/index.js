038cb593502436fac8f3fcdfb2066438c54446da
Norbert Aluba Dela Pena <norbert@sg.ibm.com>
2017-12-30 03:07:00 +0800

    Add app logging to controllers

--- api/categories/index.js
+++ api/categories/index.js
@@ -1,23 +1,26 @@
 const Category = require('./category');
+const Log = require('../logs/log');
 
 const category = {};
 
 category.listAllCategories = (req, res) => {
-  const objCategory = new Category({});
-  objCategory.findStructuredAll()
+  new Log({ message: 'CATEGORY_LIST_STRUCTURED', type: 'INFO' }).create();
+  new Category({}).findStructuredAll()
     .then(result => res.json(result))
-    .catch(err => res.status(err === 'Not Found' ? 404 : 500).json({
-      message: err === 'Not Found' ? 'Not found' : err,
-    }));
+    .catch((err) => {
+      new Log({ message: `CATEGORY_LIST_STRUCTURED ${err}`, type: 'ERROR' }).create();
+      return res.status(err === 'Not Found' ? 404 : 500).json({ message: err === 'Not Found' ? 'Not found' : err });
+    });
 };
 
 category.addCategory = (req, res) => {
-  const objCategory = new Category(req.swagger.params.body.value);
-  objCategory.create()
+  new Log({ message: 'CATEGORY_ADD', type: 'INFO' }).create();
+  new Category(req.swagger.params.body.value).create()
     .then(id => res.json({ id, message: 'Saved' }))
-    .catch(err => res.status(err === 'Found' ? 201 : 500).json({
-      message: err === 'Found' ? 'Existing' : 'Failed',
-    }));
+    .catch((err) => {
+      new Log({ message: `CATEGORY_ADD ${err}`, type: 'ERROR' }).create();
+      return res.status(err === 'Found' ? 201 : 500).json({ message: err === 'Found' ? 'Existing' : 'Failed' });
+    });
 };
 
 module.exports = category;
--- api/orders/index.js
+++ api/orders/index.js
@@ -5,36 +5,33 @@ const Log = require('../logs/log');
 const orderItem = {};
 
 orderItem.addOrderItem = (req, res) => {
-  new Log({ message: 'ADD_ORDER_ITEM', type: 'INFO' }).create();
-  const objOrder = new Order(req.swagger.params.body.value);
-  objOrder.create()
+  new Log({ message: 'ORDER_ITEM_ADD', type: 'INFO' }).create();
+  new Order(req.swagger.params.body.value).create()
     .then(id => res.json({ id, message: 'Saved' }))
     .catch((err) => {
-      new Log({ message: `ADD_ORDER_ITEM ${err}`, type: 'ERROR' }).create();
+      new Log({ message: `ORDER_ITEM_ADD ${err}`, type: 'ERROR' }).create();
       return res.status(err === 'Found' ? 201 : 500).json({ message: err === 'Found' ? 'Existing' : err });
     });
 };
 
 orderItem.updateOrderItem = (req, res) => {
-  new Log({ message: 'UPDATE_ORDER_ITEM', type: 'INFO' }).create();
-  const objOrder = new Order(req.swagger.params.body.value);
-  objOrder.update(query.validateParam(req.swagger.params, 'orderId', 0))
+  new Log({ message: 'ORDER_ITEM_UPDATE', type: 'INFO' }).create();
+  new Order(req.swagger.params.body.value).update(query.validateParam(req.swagger.params, 'orderId', 0))
     .then(msg => res.json({ message: `Updated ${msg}` }))
     .catch((err) => {
-      new Log({ message: `UPDATE_ORDER_ITEM ${err}`, type: 'ERROR' }).create();
+      new Log({ message: `ORDER_ITEM_UPDATE ${err}`, type: 'ERROR' }).create();
       return res.status(err === 'Not found' ? 404 : 500).json({ message: err === 'Not found' ? 'Not found' : 'Failed' });
     });
 };
 
 orderItem.getOrderItems = (req, res) => {
-  new Log({ message: 'GET_ORDER_ITEMS', type: 'INFO' }).create();
-  const objOrder = new Order({});
-  objOrder.findAll(query.validateParam(req.swagger.params, 'skip', 0), query.validateParam(req.swagger.params, 'limit', 10), {
+  new Log({ message: 'ORDER_ITEM_GET', type: 'INFO' }).create();
+  new Order({}).objOrder.findAll(query.validateParam(req.swagger.params, 'skip', 0), query.validateParam(req.swagger.params, 'limit', 10), {
     session_id: query.validateParam(req.swagger.params, 'key', 0),
   })
     .then(result => res.json(result))
     .catch((err) => {
-      new Log({ message: `GET_ORDER_ITEMS ${err}`, type: 'ERROR' }).create();
+      new Log({ message: `ORDER_ITEM_GET ${err}`, type: 'ERROR' }).create();
       return res.status(err === 'Not found' ? 404 : 500).json({ message: err === 'Not found' ? 'Not found' : 'Failed' });
     });
 };
