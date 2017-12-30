b11edd95caa7f98b759922f78900cd2559378d9d
Norbert Aluba Dela Pena <norbert@sg.ibm.com>
2017-12-30 20:53:31 +0800

    Add temp creds to develop

--- config/config.js
+++ config/config.js
@@ -6,10 +6,10 @@ module.exports = {
     hostname: 'localhost',
   },
   db: {
-    hostname: process.env.DB_HOSTNAME || 'localhost',
+    hostname: process.env.DB_HOSTNAME || 'madbgrocerx01.cvz1mnpjqgvm.ap-southeast-1.rds.amazonaws.com',
     port: process.env.DB_PORT || 3306,
     username: process.env.DB_USERNAME || 'grocerx',
-    password: process.env.DB_PASSWORD,
+    password: process.env.DB_PASSWORD || 'Mast3rrS10WAR',
     name: process.env.DB_NAME || 'grocerystore',
   },
   swaggerFile: 'api/swagger/swagger.yaml',
--- service/connection.js
+++ service/connection.js
@@ -2,6 +2,8 @@ const config = require('../config/config');
 // const log = require('color-logs')(true, true, __filename);
 const sqlModel = require('mysql-model');
 
+console.log(config.db);
+
 module.exports = sqlModel.createConnection({
   host: config.db.hostname,
   user: config.db.username,
