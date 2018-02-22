de0287144c002b9e5b5cb1b24d46ee07a2faef5b
Norbert Aluba Dela Pena <norbs.knight@gmail.com>
2018-02-22 12:27:01 +0800

    Add initial gc module

---
+++ api/gc/gc.js
@@ -0,0 +1,143 @@
+const BluePromise = require('bluebird');
+const _ = require('lodash');
+const sql = require('sql');
+const ConnNew = require('../../service/connectionnew');
+
+const log = require('color-logs')(true, true, 'Item');
+
+let that;
+
+/**
+  * List constructor
+  * @param {object} item
+  * @return {object}
+*/
+function Gc(gc) {
+  sql.setDialect('mysql');
+
+  this.model = _.extend(gc, {
+    dateCreated: new Date().getTime(),
+    dateUpdated: new Date().getTime(),
+  });
+  this.table = 'giftcertificate';
+  this.dbConnNew = ConnNew;
+  this.sqlTable = sql.define({
+    name: this.table,
+    columns: [
+      'id',
+      'code',
+      'amount',
+      'expiryDate',
+      'dateCreated',
+      'dateUpdated',
+      'status',
+    ],
+  });
+
+  that = this;
+}
+
+/**
+  * create
+  * @return {object/number}
+*/
+Gc.prototype.create = () => new BluePromise((resolve, reject) => {
+  const query = that.sqlTable.insert(that.model).toQuery();
+  that.dbConnNew.queryAsync(query.text, query.values)
+    .then((response) => {
+      resolve(response.insertId);
+    })
+    .catch((err) => {
+      reject(err);
+    });
+});
+
+/**
+  * update
+  * @param {string} id
+  * @return {object/number}
+*/
+Gc.prototype.update = code => new BluePromise((resolve, reject) => {
+  that.model.dateUpdated = new Date().getTime();
+  that.getByValue(code, 'code')
+    .then((resultList) => {
+      if (!resultList[0].id) {
+        reject('Not foundssss');
+      } else {
+        that.model = _.merge(resultList[0], that.model);
+        const query = that.sqlTable.update(that.model)
+          .where(that.sqlTable.id.equals(resultList[0].id)).toQuery();
+        that.dbConnNew.queryAsync(query.text, query.values)
+          .then((response) => {
+            resolve(response.message);
+          })
+          .catch((err) => {
+            reject(err);
+          });
+      }
+    })
+    .catch((err) => {
+      reject(err);
+    });
+});
+
+/**
+  * findAll
+  * @param {string} limit
+  * @param {string} offset
+  * @return {object}
+*/
+Gc.prototype.findAll = (skip, limit, filters) => {
+  let query = null;
+  if (filters.something) {
+    query = that.sqlTable
+      .select(that.sqlTable.star())
+      .from(that.sqlTable)
+      .limit(limit)
+      .offset(skip)
+      .toQuery();
+  } else {
+    query = that.sqlTable
+      .select(that.sqlTable.star())
+      .from(that.sqlTable)
+      .limit(limit)
+      .offset(skip)
+      .toQuery();
+  }
+  log.info(query.text);
+
+  return that.dbConnNew.queryAsync(query.text, query.values);
+};
+
+/**
+  * findById
+  * @param {string} limit
+  * @param {string} offset
+  * @return {object}
+*/
+Gc.prototype.findById = id => that.getByValue(id, 'id');
+Gc.prototype.getById = id => that.getByValue(id, 'id');
+
+/**
+  * Get by value
+  * @param {any} value
+  * @param {string} field
+  * @return {object<Promise>}
+*/
+Gc.prototype.getByValue = (value, field) => {
+  const query = that.sqlTable
+    .select(that.sqlTable.star())
+    .from(that.sqlTable)
+    .where(that.sqlTable[field].equals(value)).toQuery();
+  return that.dbConnNew.queryAsync(query.text, query.values);
+};
+
+/**
+  * Release connection
+  * @param {any} value
+  * @param {string} field
+  * @return {object<Promise>}
+*/
+Gc.prototype.release = () => that.dbConnNew.releaseConnectionAsync();
+
+module.exports = Gc;
---
+++ api/gc/index.js
@@ -0,0 +1,55 @@
+const query = require('../../service/query');
+const Gc = require('./gc');
+// const log = require('color-logs')(true, true, '');
+const Log = require('../logs/log');
+
+const gc = {};
+
+
+/**
+* Update
+* @param {Object} req
+* @param {Object} res
+* @return {Object}
+*/
+gc.updateGc = (req, res) => {
+  new Log({ message: 'GC_UPDATE', type: 'INFO' }).create();
+  const instGc = new Gc(req.swagger.params.body.value);
+  instGc.update(query.validateParam(req.swagger.params, 'code', ''))
+    .then(msg => res.json({ message: `Updated ${msg}` }))
+    .catch((err) => {
+      console.log(err);
+      new Log({ message: `GC_UPDATE ${err}`, type: 'ERROR' }).create();
+      return res.status(err === 'Not found' ? 404 : 500).json({ message: err === 'Found' ? 'Existing' : 'Failed' });
+    })
+    .finally(() => {
+      instGc.release();
+    });
+};
+
+/**
+* Get
+* @param {Object} req
+* @param {Object} res
+* @return {Object}
+*/
+gc.getGc = (req, res) => {
+  new Log({ message: 'GC_GET', type: 'INFO' }).create();
+  const instGc = new Gc({});
+  instGc.getByValue(query.validateParam(req.swagger.params, 'code', ''), 'code')
+    .then((resultList) => {
+      if (resultList.length === 0) {
+        return res.status(404).json({ message: 'Not found' });
+      }
+      return res.json(resultList[0]);
+    })
+    .catch((err) => {
+      new Log({ message: `GC_GET ${err}`, type: 'ERROR' }).create();
+      return res.status(err === 'Found' ? 201 : 500).json({ message: err === 'Found' ? 'Existing' : err });
+    })
+    .finally(() => {
+      instGc.release();
+    });
+};
+
+module.exports = gc;
