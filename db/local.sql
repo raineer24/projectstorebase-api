d9df657bf1cef132909563c73921ffb52692da56
Victor Michael Queja <victor.queja@gmail.com>
2018-02-22 11:07:37 +0800

    Create gc table

--- db/local.sql
+++ db/local.sql
@@ -659,6 +659,32 @@ LOCK TABLES `userprofile` WRITE;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
+--
+-- Table structure for table `giftcertificate`
+--
+
+DROP TABLE IF EXISTS `giftcertificate`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `giftcertificate` (
+  `id` bigint(50) NOT NULL AUTO_INCREMENT,
+  `code` char(50) NOT NULL,
+  `amount` char(4) NOT NULL,
+  `expiryDate` bigint(50) NOT NULL,
+  `status` char(10) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf16;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `giftcertificate`
+--
+
+LOCK TABLES `giftcertificate` WRITE;
+INSERT INTO `giftcertificate` VALUES (1,'1111','100',1522454400000,'good');
+/*!40000 ALTER TABLE `giftcertificate` ENABLE KEYS */;
+UNLOCK TABLES;
+
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
