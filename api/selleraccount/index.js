5d0c6668dd6f70d950a785c58dd44e12f33aa187
Norbert Aluba Dela Pena <norbs.knight@gmail.com>
2018-03-31 01:12:39 +0800

    Refactor Selleraccount controller

--- api/selleraccount/index.js
+++ api/selleraccount/index.js
@@ -1,6 +1,6 @@
 // const BluePromise = require('bluebird');
 // const Conn = require('../../service/connection');
-const Selleracct = require('./selleraccount');
+const Selleraccount = require('./selleraccount');
 const query = require('../../service/query');
 const Log = require('../logs/log');
 
@@ -13,12 +13,16 @@ const selleraccount = {};
 * @return {Object}
 */
 selleraccount.registerAccount = (req, res) => {
-  new Log({ message: 'SELLER_ACCT_CREATE', type: 'INFO' }).create();
-  new Selleracct(req.swagger.params.body.value).create()
+  new Log({ message: 'SELLER_ACCOUNT_CREATE', type: 'INFO' }).create();
+  const instSellerAccount = new Selleraccount(req.swagger.params.body.value);
+  instSellerAccount.create()
     .then(id => res.json({ id, message: 'Saved' }))
     .catch((err) => {
-      new Log({ message: `SELLER_ACCT_CREATE ${err}`, type: 'ERROR' }).create();
+      new Log({ message: `SELLER_ACCOUNT_CREATE ${err}`, type: 'ERROR' }).create();
       return res.status(err === 'Found' ? 201 : 500).json({ message: err === 'Found' ? 'Existing' : 'Failed' });
+    })
+    .finally(() => {
+      instSellerAccount.release();
     });
 };
 
@@ -29,18 +33,18 @@ selleraccount.registerAccount = (req, res) => {
 * @return {Object}
 */
 selleraccount.getAllSellerUsers = (req, res) => {
-  new Log({ message: 'SELLER_ACCT_USER_LIST', type: 'INFO' }).create();
-  const instSelleracct = new Selleracct({});
-  instSelleracct.findAll(query.validateParam(req.swagger.params, 'skip', 0), query.validateParam(req.swagger.params, 'limit', 10), {
+  new Log({ message: 'SELLER_ACCOUNT_USER_LIST', type: 'INFO' }).create();
+  const instSellerAccount = new Selleraccount({});
+  instSellerAccount.findAll(query.validateParam(req.swagger.params, 'skip', 0), query.validateParam(req.swagger.params, 'limit', 10), {
     useraccountId: query.validateParam(req.swagger.params, 'useraccountId', 0),
   })
     .then(result => res.json(result))
     .catch((err) => {
-      new Log({ message: `ORDER_LIST ${err}`, type: 'ERROR' }).create();
+      new Log({ message: `SELLER_ACCOUNT_USER_LIST ${err}`, type: 'ERROR' }).create();
       return res.status(err === 'Not found' ? 404 : 500).json({ message: err === 'Not found' ? 'Not found' : 'Failed' });
     })
     .finally(() => {
-      instSelleracct.release();
+      instSellerAccount.release();
     });
 };
 
@@ -51,12 +55,16 @@ selleraccount.getAllSellerUsers = (req, res) => {
 * @return {Object}
 */
 selleraccount.updateAccount = (req, res) => {
-  new Log({ message: 'SELLER_ACCT_UPDATE', type: 'INFO' }).create();
-  new Selleracct(req.swagger.params.body.value).update(query.validateParam(req.swagger.params, 'id', 0))
+  new Log({ message: 'SELLER_ACCOUNT_UPDATE', type: 'INFO' }).create();
+  const instSellerAccount = new Selleraccount(req.swagger.params.body.value);
+  instSellerAccount.update(query.validateParam(req.swagger.params, 'id', 0))
     .then(status => res.json({ status, message: 'Updated' }))
     .catch((err) => {
-      new Log({ message: `SELLER_ACCT_UPDATE ${err}`, type: 'ERROR' }).create();
+      new Log({ message: `SELLER_ACCOUNT_UPDATE ${err}`, type: 'ERROR' }).create();
       return res.status(err === 'Not Found' ? 404 : 500).json({ message: err === 'Not Found' ? 'Not found' : 'Failed' });
+    })
+    .finally(() => {
+      instSellerAccount.release();
     });
 };
 
@@ -68,12 +76,16 @@ selleraccount.updateAccount = (req, res) => {
 * @return {Object}
 */
 selleraccount.viewAccount = (req, res) => {
-  new Log({ message: 'SELLER_ACCT_GET', type: 'INFO' }).create();
-  new Selleracct().getById(query.validateParam(req.swagger.params, 'id', 0))
-    .then(result => res.json(Selleracct.cleanResponse(result, { message: 'Found' })))
+  new Log({ message: 'SELLER_ACCOUNT_GET', type: 'INFO' }).create();
+  const instSellerAccount = new Selleraccount();
+  instSellerAccount.getById(query.validateParam(req.swagger.params, 'id', 0))
+    .then(result => res.json(Selleraccount.cleanResponse(result, { message: 'Found' })))
     .catch((err) => {
-      new Log({ message: `SELLER_ACCT_GET ${err}`, type: 'ERROR' }).create();
+      new Log({ message: `SELLER_ACCOUNT_GET ${err}`, type: 'ERROR' }).create();
       return res.status(404).json({ message: 'Not found' });
+    })
+    .finally(() => {
+      instSellerAccount.release();
     });
 };
 
